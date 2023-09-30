fc0b9470bc3557b838f4fb3f3008af94b19e3ce3
zlatin-r <zlatinrusev@gmail.com>
2023-09-30 14:55:52 +0200

    Solution to the task

--- First Steps In Coding - Exercise/basketball_equipment.js
+++ First Steps In Coding - Exercise/basketball_equipment.js
@@ -1,12 +1,12 @@
 function basketball_equipment(input) {
     let annual_fee = Number(input[0]);
 
-    let shoes = annual_fee * 0.40;
+    let shoes = annual_fee * 0.60;
     let clothes = shoes * 0.80;
     let ball = clothes * 0.25;
     let accessories = ball * 0.20;
 
-    let total_price = shoes + clothes + ball +accessories;
+    let total_price = shoes + clothes + ball + accessories + annual_fee;
 
     console.log(total_price);
 
