2d850d6d1c3ddaeed7ef2f37257c52a2d73eb98e
zlatin-r <zlatinrusev@gmail.com>
2023-09-30 13:42:22 +0200

    Solution to the task

--- First Steps In Coding - Exercise/vacation_book_list.js
+++ First Steps In Coding - Exercise/vacation_book_list.js
@@ -1,3 +1,12 @@
-function vacation_book_list() {
+function vacation_book_list(input) {
+    let number_of_pages = Number(input[0]);
+    let pages_for_hour = Number(input[1]);
+    let vacation_days = Number(input[2]);
 
+    let total_read_time = number_of_pages / pages_for_hour;
+    let hours_per_day = total_read_time / vacation_days;
+    
+    console.log(hours_per_day);
 }
+
+vacation_book_list(["212", "20", "2"])
