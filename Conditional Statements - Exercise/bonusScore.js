fa7978ddbd345231b173180363a5046170b2fe45
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2023-10-08 17:35:56 +0100

    Solution

--- Conditional Statements - Exercise/bonusScore.js
+++ Conditional Statements - Exercise/bonusScore.js
@@ -1,3 +1,25 @@
 function bonusScore(input) {
+    let points = Number(input[0]);
+    let bonusPoints = 0.0;
+
+    if (points <= 100) {
+        bonusPoints = 5;
+    } else if (points <= 1000) {
+        bonusPoints = points * 0.20;
+    } else {
+        bonusPoints = points * 0.10;
+    }
+
+    if (points % 2 === 0) {
+        bonusPoints = bonusPoints + 1;
+    } else if (points % 10 === 5) {
+            bonusPoints = bonusPoints + 2;
+    }
     
+    let total_points = points + bonusPoints;
+    
+    console.log(bonusPoints);
+    console.log(total_points);
 }
+
+bonusScore(["20"])
--- Conditional Statements - Exercise/sumSeconds.js
+++ Conditional Statements - Exercise/sumSeconds.js
@@ -2,6 +2,7 @@ function sumSeconds(input) {
     let firstTime = Number(input[0]);
     let secondTime = Number(input[1]);
     let thirdTime = Number(input[2]);
+
     let total_time = firstTime + secondTime + thirdTime;
     let minutes = Math.floor(total_time / 60);
     let seconds = total_time % 60;
@@ -9,7 +10,7 @@ function sumSeconds(input) {
     if (seconds < 10) {
         console.log(`${minutes}:0${seconds}`);
     } else {
-        console.log(`${minutes}:${seconds}`)
+        console.log(`${minutes}:${seconds}`);
     }
 }   
 
