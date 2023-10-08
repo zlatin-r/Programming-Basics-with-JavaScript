65f235a49acb1ba9adf6ebd5ae25629df2ff1447
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2023-10-08 16:06:29 +0100

    Solution

--- Conditional Statements - Lab/number100_200.js
+++ Conditional Statements - Lab/number100_200.js
@@ -3,7 +3,7 @@ function number100_200(input) {
 
     if (num < 100) {
         console.log("Less than 100");
-    } else if (num < 200) {
+    } else if (num <= 200) {
         console.log("Between 100 and 200");
     }   else {
         console.log("Greater than 200");
