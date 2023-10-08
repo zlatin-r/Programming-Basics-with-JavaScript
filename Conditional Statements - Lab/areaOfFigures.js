522d56a7bcb53c0afe5c994ee334f4be00aec4b4
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2023-10-08 16:27:37 +0100

    Solution

---
+++ Conditional Statements - Lab/areaOfFigures.js
@@ -0,0 +1,23 @@
+function areaOfFigures(input) {
+    let figure = input[0]
+    let area = 0
+
+    if (figure === "square") {
+        let sideA = Number(input[1]);
+        area = sideA * sideA;
+    } else if (figure === "rectangle") {
+        let sideA = Number(input[1]);
+        let sideB = Number(input[2]);
+        area = sideA * sideB;
+    } else if (figure === "circle") {
+        r = Number(input[1]);
+        area = Math.PI * (Math.pow(r, 2));
+    } else if (figure === "triangle") {
+        side = Number(input[1]);
+        h = Number(input[2]);
+        area = (side * h) / 2;
+    }
+    console.log(area.toFixed(3));
+}
+
+areaOfFigures(["circle", "6"])
