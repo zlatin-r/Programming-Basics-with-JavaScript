a3fda60c7576f9111099c0551ea6b17a10c5c95b
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2023-10-08 15:48:52 +0100

    Solution

--- Conditional Statements - Lab/passwordGuess.js
+++ Conditional Statements - Lab/passwordGuess.js
@@ -1,3 +1,12 @@
-function passwordGuess() {
-    
+function passwordGuess(input) {
+    let password = "s3cr3t!P@ssw0rd";
+    let gues = input[0];
+
+    if (password === gues) {
+        console.log("Welcome");
+    } else {
+        console.log("Wrong password!")
+    }
 }
+
+passwordGuess(["s3cr3t!P@ssw0rd"])
