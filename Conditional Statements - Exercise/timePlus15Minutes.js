ede2bd8acd015da4b745eb277436bbd7a63c0065
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2023-10-08 18:00:09 +0100

    Solution

--- Conditional Statements - Exercise/timePlus15Minutes.js
+++ Conditional Statements - Exercise/timePlus15Minutes.js
@@ -1,3 +1,21 @@
 function timePlus15Minutes(input) {
-    
+    let hour = Number(input[0]);
+    let minutes = Number(input[1]) + 15;
+
+    if (minutes >= 60) {
+        hour += 1
+        minutes -= 60
+    } 
+    if (hour > 23) {
+        hour = 0
+    }
+
+    if (minutes < 10) {
+        console.log(`${hour}:0${minutes}`)
+    } else {
+        console.log(`${hour}:${minutes}`)
+    }
+
 }
+
+timePlus15Minutes(["0", "01"])
