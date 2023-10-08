616d4902012b46b239e400a23f9dc1e14c2f93ce
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2023-10-08 18:41:49 +0100

    Solution

--- Conditional Statements - Exercise/toyShop.js
+++ Conditional Statements - Exercise/toyShop.js
@@ -1,10 +1,9 @@
 function toyShop(input) {
     let puzzle = 2.60;
-    let doll = 3 ;
+    let doll = 3;
     let teddy_bear = 4.10;
     let mignon = 8.20;
     let truck = 2;
-    let discount = 0
 
     let vaccantionPrice = Number(input[0]);
     let numberPuzzle = Number(input[1]);
@@ -14,21 +13,21 @@ function toyShop(input) {
     let numberTrucks = Number(input[5]);
     
 
-    total_ordered = numberPuzzle + numberDolls + numberTeddyBears + numberMignons + numberTrucks
-    total_bill = (numberPuzzle * puzzle) + (numberDolls * doll) +(numberTeddyBears * teddy_bear) + (numberMignons * mignon) + (numberTrucks * truck)
+    let total_ordered = numberPuzzle + numberDolls + numberTeddyBears + numberMignons + numberTrucks;
+    let total_bill = (numberPuzzle * puzzle) + (numberDolls * doll) +(numberTeddyBears * teddy_bear) + (numberMignons * mignon) + (numberTrucks * truck);
 
     if (total_ordered >= 50) {
-        discount = total_bill * 0.25
+        let discount = total_bill * 0.25
+        total_bill -= discount 
     }
 
-    let rent = (total_bill - discount) * 0.10
-    let total = (total_bill - discount) - rent
-    let abs_total = total - vaccantionPrice
+    let rent = total_bill * 0.10;
+    total_bill -= rent;
 
-    if (total > vaccantionPrice) {
-        console.log(`Yes! ${abs_total.toFixed(2)} lv left.`)
+    if (total_bill >= vaccantionPrice) {
+        console.log(`Yes! ${(total_bill - vaccantionPrice).toFixed(2)} lv left.`);
     } else {
-        console.log(`Not enough money! ${abs_total.toFixed(2)} lv needed.`)
+        console.log(`Not enough money! ${(vaccantionPrice - total_bill).toFixed(2)} lv needed.`);
     }
 
 }
@@ -39,3 +38,4 @@ toyShop(["40.8",
 "30",
 "50",
 "10"])
+
