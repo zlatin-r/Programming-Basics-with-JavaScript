88258a961ae175cd18759efd136b411db1c52b46
zlatin-r <zlatinrusev@gmailcom>
2023-10-12 17:47:07 +0200

    Solution

--- Conditional Statements - Exercise/lunchBreak.js
+++ Conditional Statements - Exercise/lunchBreak.js
@@ -1,3 +1,20 @@
 function lunchBreak(input) {
-    
+    let seriesName = input[0];
+    let episodeDuration = Number(input[1]);
+    let restDuration = Number(input[2]);
+
+    let lunchDuration = restDuration / 8
+    let leisureTime = restDuration / 4
+
+    let timeHave = restDuration - (lunchDuration + leisureTime)
+
+    if (timeHave >= episodeDuration) {
+        let timeLeft = timeHave - episodeDuration
+        console.log(`You have enough time to watch ${seriesName} and left with ${Math.ceil(timeLeft)} minutes free time.`)
+    } else {
+        let timeNeeded = episodeDuration - timeHave
+        console.log(`You don't have enough time to watch ${seriesName}, you need ${Math.ceil(timeNeeded)} more minutes.`)
+    }
 }
+
+lunchBreak(["Game of Thrones", "60", "96"])
