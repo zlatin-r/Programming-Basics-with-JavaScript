b117895db70963e173b414c4ca972079f7c3f81c
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-10-23 16:56:07 +0200

    Solution

--- For Loop - Lab/vowelsSum.js
+++ For Loop - Lab/vowelsSum.js
@@ -1,3 +1,19 @@
-function vowelsSum() {
-    
+function vowelsSum(input) {
+    let text = input[0];
+    let sum = 0;
+
+    for (let i = 0; i < text.length; i++) {
+        let ch = text[i];
+        
+        switch(ch){
+            case ch = "a": sum += 1; break;
+            case ch = "e": sum += 2; break;
+            case ch = "i": sum += 3; break;
+            case ch = "o": sum += 4; break;
+            case ch = "u": sum += 5; break;
+        }
+    }
+    console.log(sum);
 }
+
+vowelsSum(["hello"])
