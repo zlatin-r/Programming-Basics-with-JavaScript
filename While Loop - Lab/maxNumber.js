fe1e4a5f4ae9bd71825bbb870a772fc73f5219a4
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-10-28 13:38:52 +0200

    Solution

--- While Loop - Lab/maxNumber.js
+++ While Loop - Lab/maxNumber.js
@@ -1,3 +1,25 @@
 function maxNumber(input) {
+    let index = 0;
+    let command = input[index];
+    index++;
 
+    let max_num = Number.MIN_SAFE_INTEGER;
+    
+    while (command !== "Stop") {
+        let num = Number(command);
+
+        if (num > max_num) {
+            max_num = num;
+        }
+
+        command = input[index];
+        index++;
+    }
+    console.log(max_num)
 }
+
+maxNumber(["100",
+"99",
+"80",
+"70",
+"Stop"])
