a150210205e9dd83db0c6e9d0b53eb9c2f5e7e27
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-10-28 13:24:00 +0200

    Solution

--- While Loop - Lab/accountBalance.js
+++ While Loop - Lab/accountBalance.js
@@ -1,3 +1,22 @@
 function accountBalance(input) {
-    
+    let index = 0;
+    let balance = 0;
+    let operation = input[index];
+
+    while (operation !== "NoMoreMoney") {
+        let installment = Number(operation);
+        if (installment <= 0) {
+            console.log("Invalid operation!");
+            break;
+        } else {
+            balance += installment;
+            console.log(`Increase: ${installment.toFixed(2)}`);
+            index++;
+            operation = input[index];
+        }
+    } console.log(`Total: ${balance.toFixed(2)}`);
 }
+
+accountBalance(["120",
+"45.55",
+"-150"])
