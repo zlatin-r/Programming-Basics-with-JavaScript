e0a3ad5be61b02296512f1a01fea080930d4747e
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-10-28 13:42:01 +0200

    Solution

---
+++ While Loop - Lab/garduation.js
@@ -0,0 +1,3 @@
+function garduation(input) {
+    
+}
--- While Loop - Lab/minNumber.js
+++ While Loop - Lab/minNumber.js
@@ -1,3 +1,26 @@
 function minNumber(input) {
     
+    let index = 0;
+    let command = input[index];
+    index++;
+    
+    let min_num = Number.MAX_SAFE_INTEGER;
+        
+    while (command !== "Stop") {
+        let num = Number(command);
+    
+        if (num < min_num) {
+            min_num = num;
+        }
+    
+        command = input[index];
+        index++;
+    }
+    console.log(min_num)
 }
+    
+    minNumber(["100",
+    "99",
+    "80",
+    "70",
+    "Stop"])
