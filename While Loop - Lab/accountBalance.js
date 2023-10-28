9d743e37d8f4c5c927f87d08418140d42334d3f6
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-10-28 13:11:24 +0200

    Solution

---
+++ While Loop - Lab/accountBalance.js
@@ -0,0 +1,3 @@
+function accountBalance(input) {
+    
+}
--- While Loop - Lab/sequence2k1.js
+++ While Loop - Lab/sequence2k1.js
@@ -1,3 +1,12 @@
 function sequence2k1(input) {
-    
+    let index = 0;
+    let n = input[0];
+    let k = 1;
+
+    while (k <= n) {
+        console.log(k);
+        k = 2 * k + 1
+    }
 }
+
+sequence2k1(["31"])
