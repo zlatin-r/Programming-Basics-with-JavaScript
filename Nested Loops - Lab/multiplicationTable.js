62a6c7e15b3b2b20fe82452b4b1396b6c83dd6c0
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-04 14:02:53 +0100

    Solution

--- Nested Loops - Lab/multiplicationTable.js
+++ Nested Loops - Lab/multiplicationTable.js
@@ -1,3 +1,9 @@
 function multiplicationTable() {
-    
+    for (let a = 1; a <= 10; a++) {
+        for (let b = 1; b <= 10; b++) {
+            console.log(`${a} * ${b} = ${a * b}`);
+        }
+    }
 }
+
+multiplicationTable()
