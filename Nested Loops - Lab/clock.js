5683f292d950790df225a892c121f597c7f48ea2
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-04 13:59:02 +0100

    Solution

--- Nested Loops - Lab/clock.js
+++ Nested Loops - Lab/clock.js
@@ -1,3 +1,9 @@
-function clock(input) {
-    
+function clock() {
+    for (let h = 0; h < 24; h++) {
+        for (let m = 0; m < 60; m++) {
+            console.log(`${h}:${m}`);
+        }
+    }
 }
+
+clock()
