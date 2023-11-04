4998354915114b76e96fb7596a886ca71c72a74b
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-04 15:28:11 +0100

    Solution

--- Nested Loops - Lab/building.js
+++ Nested Loops - Lab/building.js
@@ -1,3 +1,22 @@
 function building(input) {
-    
+    let flours = Number(input[0]);
+    let rooms = Number(input[1]);
+
+    for (i = flours; i > 0; i--) {
+        let buff = "";
+        for (x = 0; x < rooms; x++) {
+            if (i === flours) {
+                buff += `L${i}${x} `;
+            } else if (i % 2 === 0) {
+                buff += `O${i}${x} `;
+            } else {
+                buff += `A${i}${x} `;
+            }
+        }
+        console.log(buff);
+    }
 }
+
+building(["6", 
+        "4"])
+        
