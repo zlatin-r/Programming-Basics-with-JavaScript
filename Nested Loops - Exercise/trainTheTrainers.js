008060cdfc48c136caec928218d734e85334bfa3
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-05 16:35:02 +0100

    Solution

--- Nested Loops - Exercise/trainTheTrainers.js
+++ Nested Loops - Exercise/trainTheTrainers.js
@@ -1,3 +1,38 @@
 function trainTheTrainers(input) {
-    
+    let index = 0;
+    let judges = Number(input[index]);
+    index++;
+    let command = input[index];
+    index++;
+    let totalGrades = 0;
+    let assessmentCount = 0;
+
+    while (command !== "Finish") {
+        let sumGrades = 0;
+        let assessment = command;
+        
+        for (let i = 0; i < judges; i++) {
+            let grade = Number(input[index]);
+            assessmentCount++;
+            sumGrades += grade;
+            index++;
+        }
+        totalGrades +=  sumGrades;
+        console.log(`${assessment} - ${(sumGrades/judges).toFixed(2)}.`);
+        command = input[index];
+        index++;
+    }
+    console.log(`Student's final assessment is ${(totalGrades/assessmentCount).toFixed(2)}.`);
 }
+
+trainTheTrainers(["2",
+"Objects and Classes",
+"5.77",
+"4.23",
+"Dictionaries",
+"4.62",
+"5.02",
+"RegEx",
+"2.88",
+"3.42",
+"Finish"])
