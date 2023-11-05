722c2c0cfe0d698c4a6ce6dea68b23648673aac2
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-05 16:57:57 +0100

    Solution

--- Nested Loops - Exercise/specialNumbers.js
+++ Nested Loops - Exercise/specialNumbers.js
@@ -1,3 +1,22 @@
 function specialNumbers(input) {
+    let number = input[0];
+    let buff = "";
     
+    for (let i = 1111; i < 9999; i++) {
+        let stringI = String(i);
+        let isSpecial = true;
+        for (let j = 0; j < stringI.length; j++) {
+            let currNumber = Number(stringI[j]);
+            if (number % currNumber !== 0) {
+                isSpecial = false;
+                break;
+            }
+        }
+        if (isSpecial) {
+            buff += stringI + " ";
+        }
+    }
+    console.log(buff);
 }
+
+specialNumbers(["16"])
