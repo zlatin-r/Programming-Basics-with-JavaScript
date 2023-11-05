f25551b14b72d045254ed657e16cef3c62f96e98
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-05 16:14:15 +0100

    Solution

--- Nested Loops - Exercise/sumPrimeNonPrime.js
+++ Nested Loops - Exercise/sumPrimeNonPrime.js
@@ -1,3 +1,42 @@
 function sumPrimeNonPrime(input) {
-    
+    let index = 0;
+    let command = input[index];
+    index++;
+    let sumPrimes = 0;
+    let sumNonPrimes = 0;
+    let isPrime = true;
+
+    while (command != "stop") {
+        let currentNum = Number(command);
+
+        if (currentNum < 0) {
+            console.log(`Number is negative.`);
+            command = input[index];
+            index++;
+            continue;
+        } else {
+            for (let i = 2; i < currentNum; i++) {
+                if (currentNum % i === 0) {
+                    isPrime = false;
+                    break;
+                } else {
+                    isPrime = true;
+                }
+            }
+            if (isPrime) {
+                sumPrimes += currentNum;
+            } else {
+                sumNonPrimes += currentNum;
+            }
+        }
+        command = input[index];
+        index++;
+    }
+    console.log(`Sum of all prime numbers is: ${sumPrimes}`);
+    console.log(`Sum of all non prime numbers is: ${sumNonPrimes}`);
 }
+
+sumPrimeNonPrime(["0",
+"-9",
+"0",
+"stop"])
