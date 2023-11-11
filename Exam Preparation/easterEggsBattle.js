a85d7a612e4fdd8a5685968fb59f05650d6cb1a1
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-11 15:00:18 +0100

    Solution

--- Exam Preparation/easterEggsBattle.js
+++ Exam Preparation/easterEggsBattle.js
@@ -1,3 +1,41 @@
 function easterEggsBattle(input) {
-    
+    let player_one = Number(input[0]);
+    let player_two = Number(input[1]);
+    let index = 2;
+    let command = input[index];
+    index++;
+    let end = true;
+
+    while (command !== "End") {
+        if (command === "one") {
+            player_two--;
+        }
+        else if (command === "two") {
+            player_one--;
+        }
+        if (player_one === 0) {
+            console.log(`Player one is out of eggs. Player two has ${player_two} eggs left.`);
+            end = false;
+            break;
+        }
+        else if (player_two === 0) {
+            console.log(`Player two is out of eggs. Player one has ${player_one} eggs left.`);
+            end = false;
+            break;
+        }
+        command = input[index];
+        index++;
+    }
+    if (end) {
+        console.log(`Player one has ${player_one} eggs left.`);
+        console.log(`Player two has ${player_two} eggs left.`);
+    }
 }
+
+easterEggsBattle(["6",
+"3",
+"one",
+"two",
+"two",
+"one",
+"one"])
