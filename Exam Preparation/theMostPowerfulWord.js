0c2019601e8c3a1451d1c7eb3ec3168c052c1f97
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-12 12:42:38 +0100

    Solution

--- Exam Preparation/theMostPowerfulWord.js
+++ Exam Preparation/theMostPowerfulWord.js
@@ -1,3 +1,37 @@
 function theMostPowerfulWord(input) {
-    
+    let index = 0;
+    let word = input[index];
+    index++;
+    let max_sum = 0;
+    let most_powerfull = "";
+    let vowels = "aeiouyAEIOUY";
+
+    while (word != "End of words") {
+        let ascii_sum = 0;
+
+        for (let i = 0; i < word.length; i++) {
+            ascii_sum += word.charCodeAt(i);
+        }
+        if (vowels.includes(word[0])) {
+            ascii_sum *= word.length;
+        } else {
+            ascii_sum = Math.floor(ascii_sum / word.length);
+        }
+        if (ascii_sum > max_sum) {
+            max_sum = ascii_sum;
+            most_powerfull = word;
+        }
+
+        word = input[index];
+        index++;
+    }
+    console.log(`The most powerful word is ${most_powerfull} - ${max_sum}`);
 }
+
+theMostPowerfulWord(["The",
+    "Most",
+    "Powerful",
+    "Word",
+    "Is",
+    "Experience",
+    "End of words"])
