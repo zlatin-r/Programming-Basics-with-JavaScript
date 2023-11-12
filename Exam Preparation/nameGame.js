6ce2dd2602f542f156da5d8eb1232864b94aa2a7
zlatin-r <75085055+zlatin-r@users.noreply.github.com>
2023-11-12 13:08:03 +0100

    Solution

--- Exam Preparation/nameGame.js
+++ Exam Preparation/nameGame.js
@@ -1,3 +1,38 @@
 function nameGame(input) {
-    
+    let index = 0;
+    let name = input[index];
+    index++;
+    let max_points = 0;
+    let winner = "";
+
+    while (name != "Stop") {
+        let points = 0;
+        for (let i = 0; i < name.length; i++) {
+            let numer = Number(input[index]);
+            index++;
+            if (name.charCodeAt(i) === numer) {
+                points += 10;
+            } else {
+                points += 2;
+            }
+        }
+        if (points >= max_points) {
+            max_points = points;
+            winner = name;
+        }
+        name = input[index];
+        index++;
+    }
+    console.log(`The winner is ${winner} with ${max_points} points!`);
 }
+
+nameGame(["Ivan",
+    "73",
+    "20",
+    "98",
+    "110",
+    "Ivo",
+    "80",
+    "65",
+    "87",
+    "Stop"])
